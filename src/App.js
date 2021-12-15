856aa374f69deef4f93aeef660cd47a1f3667d95
BellaZZZZZ <bellazhao_321@qq.com>
2021-12-15 14:38:39 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Counter from './Component/Counter'
+import SearchUserList from './Component/SearchUserList'
+import Timer from './Component/Timer'
+import CustomTextInput from './Component/CustomTextInput'
 
 function App() {
   return (
     <div>
-      <Counter/>
+      <CustomTextInput/>
     </div>
   );
 }
---
+++ src/Component/SearchUserList.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect, useMemo } from 'react'
+
+const SearchUserList = () => {
+  const [users, setUsers] = useState(null)
+  const [searchKey, setSearchKey] = useState('')
+
+  useEffect(() => {
+    const doFetch = async () => {
+      // 组件首次加载时发请求获取用户数据
+      const res = await fetch('https://reqres.in/api/users/')
+      setUsers(await res.json())
+    }
+    doFetch()
+  }, [])
+
+  // let usersToShow = null
+  // if (users) {
+  //   console.log('usersToShow')
+  //   // 无论数组为何刷新，这里一定会对数组做一次过滤的操作
+  //   usersToShow = users.data.filter((user) =>
+  //     user.first_name.includes(searchKey)
+  //   )
+  // }
+
+  // 使用useMemo缓存计算的结果
+  const usersToShow = useMemo(() => {
+    console.log('usersToShow')
+    if (!users) return null
+    return users.data.filter((user) => {
+      return user.first_name.includes(searchKey)
+    })
+  }, [users, searchKey])
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchKey}
+        onChange={(evt) => setSearchKey(evt.target.value)}
+      />
+      <ul>
+        {
+          usersToShow &&
+          usersToShow.length > 0 &&
+          usersToShow.map((user) => {
+            return <li key={user.id}>{user.first_name}</li>
+          })
+        }
+      </ul>
+    </div>
+  )
+}
+
+export default SearchUserList
